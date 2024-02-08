29bf117fa0c8660b695c4575875cb1af19f279d7
Vaughn Walker <vaughn.walker@integral.io>
2024-02-08 13:05:41 -0500

    Update model to 3.5-turbo-0125

--- src/pages/api/chat-handler.js
+++ src/pages/api/chat-handler.js
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
 
-export const MODEL = "gpt-3.5-turbo";
+export const MODEL = "gpt-3.5-turbo-0125";
 
 export default async function handler(req, res) {
 	if (req.method === 'POST') {
